23edd7e89982e6dddfafd94109712c20bf8d006b
esteban <esteban@madebygrizzly.com>
2017-05-09 08:41:01 -0500

    azunc javascript

--- app/typescripts/views/home/home.ts
+++ app/typescripts/views/home/home.ts
@@ -21,24 +21,23 @@
         
         this._$container = $( '#js-frame' );
         
-        this._renderer = new PIXI.WebGLRenderer(this._$container.width(), this._$container.height(), {transparent: true} );
+        this._renderer = new PIXI.WebGLRenderer(this._$container.width() + 50, this._$container.height(), {transparent: true} );
         this._graphic = new PIXI.Graphics();
         this._stage = new PIXI.Container();
         this._ticker = new PIXI.ticker.Ticker();
         
         this._frequency_1 = 5;
-        this._width_1 = 250;
+        this._width_1 = 150;
         this._direction_1 = 0;
 
         this._frequency_2 = 5;
-        this._width_2 = 60;
+        this._width_2 = 90;
         this._direction_2 = 0;
         
         this._stage.addChild(this._graphic);
 
         this._$container.prepend(this._renderer.view);
 
-        //this._ticker.FPS = 24;
         this._ticker.add( this.animate.bind( this ));
         this._ticker.start();
     }
@@ -56,35 +55,35 @@
         //velocity
         if(this._direction_1 == 0) {
             //velociti up
-            this._frequency_1 -= .21;
-            this._width_1 += 1.3;
+            this._frequency_1 -= .31;
+            this._width_1 += 1;
         } else {
             //velociti down
-            this._frequency_1 += .21;
-            this._width_1 -= .3;
+            this._frequency_1 += .31;
+            this._width_1 -= 1;
         }
-        //wave top
-        if(this._frequency_1 < -10){ this._direction_1 = 1}
-        //wave bottom
-        if(this._frequency_1 > 10){ this._direction_1 = 0}
+        //wave max top
+        if(this._frequency_1 > 5){ this._direction_1 = 0}
+        //wave max bottom
+        if(this._frequency_1 < -5){ this._direction_1 = 1}
 
 
         if(this._direction_2 == 0) {
-            this._frequency_2 -= .1;
-            this._width_2 += 1.3;
+            this._frequency_2 -= .3;
+            this._width_2 += 1;
         } else {
-            this._frequency_2 += .1;
-            this._width_2 -= .3;
+            this._frequency_2 += .3;
+            this._width_2 -= 1;
         }
-        if(this._frequency_2 > 5){ this._direction_2 = 0}
-        if(this._frequency_2 < -5){ this._direction_2 = 1}
+        if(this._frequency_2 > 8){ this._direction_2 = 0}
+        if(this._frequency_2 < -8){ this._direction_2 = 1}
 
 
         this._graphic.clear();
 
         // waves
-        this._drawWave(this._$container.height() / 2, this._frequency_1, this._width_1, 0x5BCEE6);
-        this._drawWave((this._$container.height() / 2) + 5, this._frequency_2, this._width_2, 0x9EEAF9);
+        this._drawWave((this._$container.height() / 2 ), this._frequency_1, this._width_1, 0x5BCEE6);
+        this._drawWave((this._$container.height() / 2) + 15, this._frequency_2, this._width_2, 0x9EEAF9);
 
         this._graphic.endFill();
     }
@@ -93,14 +92,14 @@
         
         // waves        
         this._graphic.beginFill(color);
-        this._graphic.moveTo(0, posX );
-        for(var x = 0; x <= this._$container.width(); x++){
+        this._graphic.moveTo(0, posX - 20 );
+        for(var x = 0; x <= this._$container.width() + 50; x++){
 
             var y = frequency * Math.sin((Math.PI/xp) * x) + posX ;
             this._graphic.lineTo(x, y);
         }
-        this._graphic.lineTo(this._$container.width(), posX );
-        this._graphic.lineTo(this._$container.width(), this._$container.height());
+        this._graphic.lineTo(this._$container.width() + 50, posX );
+        this._graphic.lineTo(this._$container.width() + 50, this._$container.height());
         this._graphic.lineTo(0, this._$container.height());
     }
 }
